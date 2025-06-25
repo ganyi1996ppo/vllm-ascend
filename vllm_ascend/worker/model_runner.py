4919e11c4f2f582a18b994392ea430c3f1d66d48
ganyi <pleaplusone.gy@gmail.com>
2025-06-25 15:07:32 +0800

    bugfix
    Signed-off-by: ganyi <pleaplusone.gy@gmail.com>

--- vllm_ascend/worker/model_runner.py
+++ vllm_ascend/worker/model_runner.py
@@ -1333,7 +1333,7 @@ class NPUModelRunner(NPUModelRunnerBase[ModelInputForNPUWithSamplingMetadata]):
                                      self.vllm_config, virtual_engine):
                 if model_input.attn_metadata is not None:
                     model_input.attn_metadata.input_positions = model_input.input_positions
-                logger.info(f"before execute model, the embedding size is: {model_input.inputs_embeds.size()}")
+                logger.info(f"before execute model, the embedding size is: {model_input.inputs_embeds.size() if model_input is not None else None}")
                 hidden_or_intermediate_states = model_executable(
                     input_ids=model_input.input_tokens,
                     **{
